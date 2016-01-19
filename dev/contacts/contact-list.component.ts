c7b7df5c5e2e582dd964732603d1cce022990800
Piotr Kościelniak <pkoscielniak@rekord.com.pl>
2016-01-19 13:41:12 +0100

    angular2 service

--- dev/contacts/contact-list.component.ts
+++ dev/contacts/contact-list.component.ts
@@ -1,5 +1,8 @@
 import {Component} from 'angular2/core'
 import {ContactComponent} from "./contact.component";
+import {ContactService} from "./contact.service";
+import {Contact} from "./contact";
+import {OnInit} from "angular2/core";
 
 @Component({
     selector: 'contact-list',
@@ -13,36 +16,30 @@ import {ContactComponent} from "./contact.component";
     <contact [contact]="selectedContact"></contact>
     `,
     directives: [ContactComponent],
-
+    providers: [ContactService],
     styleUrls: ["../src/css/app.css"]
 })
-export class ContactListComponent {
-    public contacts = [{
-        firstName: "Max",
-        lastName: "Schwartz",
-        phone: '0137 1350 928',
-        email: 'info@schwartz.com'
-    }, {
-        firstName: "Chris",
-        lastName: "Alloy",
-        phone: '0137 555 928',
-        email: 'info@alloy.com'
-    }, {
-        firstName: "Michael",
-        lastName: "Galah",
-        phone: '0137 3333 928',
-        email: 'info@galah.com'
-    }, {
-        firstName: "Indira",
-        lastName: "Raches",
-        phone: '0137 2222 928',
-        email: 'info@raches.com'
-    }];
+export class ContactListComponent implements OnInit {
+
+    ngOnInit():any {
+        this.getContacts();
+    }
+
+    public contacts:Contact[];
+
+    constructor(private _contactService:ContactService) {
+
+    }
 
     public selectedContact = {};
 
     onSelect(contact) {
         this.selectedContact = contact;
 
+    }
+
+    getContacts() {
+        this._contactService.getContacts().then((contacts:Contact[]) =>
+            this.contacts = contacts);
     }
 }
---
+++ dev/contacts/mock-contact.ts
@@ -0,0 +1,22 @@
+import {Contact} from "./contact";
+export const CONTACTS:Contact[] = [{
+    firstName: "Max",
+    lastName: "Schwartz",
+    phone: '0137 1350 928',
+    email: 'info@schwartz.com'
+}, {
+    firstName: "Chris",
+    lastName: "Alloy",
+    phone: '0137 555 928',
+    email: 'info@alloy.com'
+}, {
+    firstName: "Michael",
+    lastName: "Galah",
+    phone: '0137 3333 928',
+    email: 'info@galah.com'
+}, {
+    firstName: "Indira",
+    lastName: "Raches",
+    phone: '0137 2222 928',
+    email: 'info@raches.com'
+}];
