af9b8be4c2deae0f06c7f7d81cd4204103324368
Piotr Kościelniak <piotrek.koscielniak@gmail.com>
2016-02-03 11:38:36 +0100

    outputs outputs

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,71 +1,19 @@
-import {Component} from 'angular2/core';
-import {ContactListComponent} from "./contacts/contact-list.component";
-import {ROUTER_DIRECTIVES} from "angular2/router";
-import {RouteConfig} from "angular2/router";
-import {NewContactComponent} from "./contacts/new-contact.component";
-import {HTTPTestComponent} from "./http-test.component";
-import {resolve} from "url";
-
+import {Component} from "angular2/core";
+import {ChildComponent} from "./child.component";
 @Component({
     selector: 'app',
     template: `
-        <header>
-            <nav>
-                <a [routerLink]="['Contacts']">Contacts</a>
-                <a [routerLink]="['NewContact']">New Contact</a>
-            </nav>
-        </header>
-        <div class="main">
-            <router-outlet></router-outlet>
-            <http-test></http-test>
-           <div class="pipes">
-                <h2>Date Pipe</h2>
-                <div>
-                    {{date | date}}
-                </div>
-                 <div>
-                    {{date | date:'y-m-d'}}
-                </div>
-                <div>
-                    {{date | date:'short'}}
-                </div>
-                <div>
-                    {{date | date:'fullDate'}}
-                </div>
-                <h2>Number Pipe</h2>
-                <div>
-                    {{4.566 | number:'1.2-2'}}
-                </div>
-                <h2>Currency Pipe</h2>
-                <div>
-                    {{15.99 | currency}}
-                </div>
-                <div>
-                    {{15.99 | currency:'EUR'}}
-                </div>
-                <div>
-                    {{15.99 | currency:'EUR':true}}
-                </div>
-                <div>
-                    {{15.99 | currency:'EUR':true:'1.4-4'}}
-                </div>
-                <h2>Stateful Pipe</h2>
-                <div>
-                    {{ randomData | async}}
-                </div>
-           </div>
+        <div class="parent">
+            <h1>Parent</h1>
+            <p>Value entered in child component {{childValue}}</p>
+            <input type="text" #parentInput (keyup)="0"><br>
+            <div class="child">
+                <child [parentValue]="parentInput.value" (childChanged)="childValue = $event"></child>
+            </div>
         </div>
     `,
-    directives: [ContactListComponent, HTTPTestComponent, ROUTER_DIRECTIVES]
+    directives: [ChildComponent]
 })
-@RouteConfig([
-    {path: '/contacts', name: 'Contacts', component: ContactListComponent, useAsDefault: true},
-    {path: '/newcontact', name: 'NewContact', component: NewContactComponent},
-    {path: '/newcontact/:lastName', name: 'NewContactFromContact', component: NewContactComponent}
-])
-export class AppComponent {
-    date = new Date();
-    randomData = new Promise((resolve, reject)=> {
-        setTimeout(() => resolve("Random data!"), 1000);
-    });
+export class AppComponent{
+
 }
---
+++ dev/child.component.ts
@@ -0,0 +1,21 @@
+import {Component,EventEmitter} from "angular2/core";
+import {Output} from "angular2/core";
+
+@Component({
+    selector: 'child',
+    template: `
+        <h2>Child</h2>
+        <p>Value entered in parent component: {{parentValue}}</p>
+        <input type="text" #childInput (keyup)="onChange(childInput.value)">
+    `,
+    inputs: ['parentValue']
+    // outputs: ['childChanged'] //to ways to do the same, it's also possible with input
+})
+export class ChildComponent {
+    parentValue:string;
+    @Output() childChanged = new EventEmitter<string>();
+
+    onChange(value:string) {
+        this.childChanged.emit(value);
+    }
+}
